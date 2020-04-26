f9dd2be70a19dba0bb8918a71ae2206d2db09ce1
JA-EMIN <jaemin.jang.jj@gmail.com>
2020-04-26 10:55:46 +0900

    IComparable and IComparer Interface

---
+++ Escort_CSharp/IComparable_IComparer/Man.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IComparable_IComparer
+{
+    class Man : IComparable
+    {
+        string name;
+        string addr;
+        public string Addr
+        {
+            get
+            {
+                return addr;
+            }
+        }
+        public Man(string name, string addr)
+        {
+            this.name = name;
+            this.addr = addr;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Man man = obj as Man;
+            if(man.name == null)
+            {
+                throw new ApplicationException("Man 형식 개체여야 합니다.");
+            }
+            else
+            {
+                return this.name.CompareTo(man.name);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}:{1}", name, addr);
+        }
+    }
+
+    class AddrComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            Man mx = x as Man;
+            Man my = y as Man;
+
+            if(mx == null || my == null)
+            {
+                throw new ApplicationException("Man형식 개체가 아닌 입력 인자가 있습니다.");
+            }
+            else
+            {
+                return mx.Addr.CompareTo(my.Addr);
+            }
+        }
+    }
+}
---
+++ Escort_CSharp/IComparable_IComparer/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+
+namespace IComparable_IComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 배열 ArrayList
+            //int[] arr = new int[10];
+            //Random random = new Random();
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = random.Next(21);
+            //}
+
+            //foreach (int i in arr)
+            //{
+            //    Console.Write("{0} ", i);
+            //}
+            //Console.WriteLine();
+            //Array.Sort(arr);
+            //Console.WriteLine("===== 정렬 후 =====");
+            //foreach (int i in arr)
+            //{
+            //    Console.Write("{0} ", i);
+            //}
+            //Console.Clear();
+
+            //ArrayList ar = new ArrayList();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    ar.Add(random.Next(21));
+            //}
+
+            //foreach (int i in ar)
+            //{
+            //    Console.Write("{0} ", i);
+            //}
+            //Console.WriteLine();
+            //ar.Sort();
+            //Console.WriteLine("===== 정렬 후 =====");
+            //foreach (int i in ar)
+            //{
+            //    Console.Write("{0} ", i);
+            //}
+            //Console.Clear();
+            #endregion
+
+            Man[] men = new Man[4];
+            men[0] = new Man("홍길동", "율도국");
+            men[1] = new Man("강감찬", "대한민국");
+            men[2] = new Man("김구", "제주도");
+            men[3] = new Man("을지문덕", "천안");
+
+            foreach (Man man in men)
+            {
+                Console.WriteLine(man);
+            }
+
+            Array.Sort(men);
+            Console.WriteLine("===== 이름순 정렬 후 =====");
+            foreach (Man man in men)
+            {
+                Console.WriteLine(man);
+            }
+
+            AddrComparer ac = new AddrComparer();
+            Array.Sort(men, ac);
+            Console.WriteLine("===== 주소순 정렬 후 =====");
+            foreach (Man man in men)
+            {
+                Console.WriteLine(man);
+            }
+
+
+        }
+    }  
+}
